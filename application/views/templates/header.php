c76c9c093cf795f7aeb453f535d0666c25bdba3d
Ahmet Altas <99040436@mydavinci.nl>
2017-10-02 12:22:21 +0200

    no message

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,6 +2,10 @@
 <html>
         <head>
                 <title>Bookshop</title>
+   	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<link rel="stylesheet" type="text/css" href="/bookshop/CSS/style.css">
         </head>
         <body>
  
